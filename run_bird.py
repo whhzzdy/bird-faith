7dadbe35b64665cbcf7f90af355daa12ac58e6c2
whhzzdy <116626027+whhzzdy@users.noreply.github.com>
2026-05-28 15:44:06 +0800

    Update run_bird.py

--- run_bird.py
+++ run_bird.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # ================= 配置区域 =================
-API_KEY = "sk-9885898f801645a890417229eeb56d78" 
+API_KEY = "" 
 BASE_URL = "https://api.deepseek.com"
 INPUT_FILE = "dev_prompt.jsonl"
 OUTPUT_FILE = "predict_dev_1.json"
